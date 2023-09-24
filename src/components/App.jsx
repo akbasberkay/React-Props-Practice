53fa310b8d955c45c0a4ad6404ada1e8e5d35260
Berkay Akbaş <68223323+askbasberkay@users.noreply.github.com>
2023-09-25 00:09:08 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+  console.log(contacts);
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        imgURL={contacts[0].imgURL}
+        phone={contacts[0].phone}
+        email={contacts[0].email}
+      />
+    </div>
+  );
+}
+
+export default App;
